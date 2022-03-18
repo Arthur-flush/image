3b203149af5dea9fa9f4ac7d5ae0a2ced6667efd
Arthur <arthur2.bossu@gmail.com>
2022-03-18 11:41:19 +0100

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ TP4/main.c
@@ -0,0 +1,376 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N 1
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+typedef enum { false, true } bool;
+
+
+
+typedef struct RGB_t {
+    uchar R, G, B;
+} RGB;
+
+typedef struct picture_t
+{
+    uchar type; /* Type de l’image (0 ou 1) */
+    uint height, width;
+    uchar value_max; /* Valeur max utilisee pour representer les pixels */
+    union
+    {
+        uchar **pixels; // un pointeur vers un tableau 2D d’unsigned char (pour les pgm)
+        RGB **pixels_rgb; // un pointeur vers un tableau 2D de pixels_rgb (pour les ppm)
+    };
+} picture;
+
+picture *get_picture(char *file_name) {
+    picture *pic = malloc(sizeof(picture));
+
+    FILE *fp = fopen(file_name, "r");
+    getc(fp);
+    char specification = getc(fp);
+    if (specification == '2') {
+        pic->type = 0;
+        getc(fp);
+
+        while (getc(fp) == '#') 
+            while (getc(fp) != '\n');
+        
+        int width, length, max;
+        fseek(fp, -1, SEEK_CUR);
+        fscanf(fp, "%d %d", &width, &length);
+        
+        fscanf(fp, "%d", &max);
+
+        pic->height = length;
+        pic->width = width;
+        pic->value_max = max;
+        
+
+        pic->pixels = malloc(sizeof(uchar*) * length);
+        if (pic->pixels == NULL) {
+            perror("error while malloc pic->pixels");
+            exit(1);
+        }
+        
+        for (int i = 0; i < length; i++) {
+            pic->pixels[i] = malloc(sizeof(uchar)*width);
+            if (pic->pixels[i] == NULL) {
+                perror("error while malloc pic->pixels[i]");
+                exit(1);
+            }
+
+
+            for (int j = 0; j < width; j++) {
+                uint c;
+                fscanf(fp, "%u", &c);
+                pic->pixels[i][j] = (uchar)c;
+            }
+        }
+        
+    }
+    if (specification == '3') {
+        pic->type = 1;
+        char c = getc(fp);
+
+        while ((c = getc(fp)) == '#') 
+            while ((c = getc(fp)) != '\n');
+        
+        uint width, length, max;
+        fseek(fp, -1, SEEK_CUR);
+        fscanf(fp, "%u %u", &width, &length);
+        
+        fscanf(fp, "%u", &max);
+
+        pic->height = length;
+        pic->width = width;
+        pic->value_max = max;
+        
+
+        pic->pixels = malloc(sizeof(uchar*) * length);
+        if (pic->pixels == NULL) {
+            perror("error while malloc pic->pixels");
+            exit(1);
+        }
+        
+        for (int i = 0; i < length; i++) {
+            pic->pixels[i] = malloc(sizeof(RGB)*width);
+            if (pic->pixels[i] == NULL) {
+                perror("error while malloc pic->pixels[i]");
+                exit(1);
+            }
+
+            
+            for (int j = 0; j < width; j++) {
+                uint R, G, B;
+                fscanf(fp, "%u %u %u", &R, &G, &B);
+                RGB pix;
+                pix.R = (uchar)R;
+                pix.G = (uchar)G; 
+                pix.B = (uchar)B;
+                pic->pixels_rgb[i][j] = pix;
+            }
+        }
+
+
+    }
+    if (specification == '5') {
+        pic->type = 0;
+        getc(fp);
+
+        while (getc(fp) == '#') 
+            while (getc(fp) != '\n');
+        
+        int width, length, max;
+        fseek(fp, -1, SEEK_CUR);
+        fscanf(fp, "%d %d", &width, &length);
+        
+        fscanf(fp, "%d", &max);
+
+        pic->height = length;
+        pic->width = width;
+        pic->value_max = max;
+        
+
+        pic->pixels = malloc(sizeof(uchar*) * length);
+        if (pic->pixels == NULL) {
+            perror("error while malloc pic->pixels");
+            exit(1);
+        }
+        fgetc(fp); // newline
+        for (int i = 0; i < length; i++) {
+            pic->pixels[i] = malloc(sizeof(uchar)*width);
+            if (pic->pixels[i] == NULL) {
+                perror("error while malloc pic->pixels[i]");
+                exit(1);
+            }
+        fread(pic->pixels[i], 1, width, fp);
+        }
+    }
+    if (specification == '6') {
+        pic->type = 1;
+        getc(fp);
+
+        while (getc(fp) == '#') 
+            while (getc(fp) != '\n');
+        
+        int width, length, max;
+        fseek(fp, -1, SEEK_CUR);
+        fscanf(fp, "%d %d", &width, &length);
+        
+        fscanf(fp, "%d", &max);
+
+        pic->height = length;
+        pic->width = width;
+        pic->value_max = max;
+        
+
+        pic->pixels = malloc(sizeof(uchar*) * length);
+        if (pic->pixels == NULL) {
+            perror("error while malloc pic->pixels");
+            exit(1);
+        }
+        fgetc(fp); // newline
+        for (int i = 0; i < length; i++) {
+            pic->pixels[i] = malloc(sizeof(RGB)*width * length);
+            if (pic->pixels[i] == NULL) {
+                perror("error while malloc pic->pixels[i]");
+                exit(1);
+            }
+            fread(pic->pixels_rgb[i], 3, width, fp);
+        }
+    }
+    fclose(fp);
+    return pic;
+}
+
+void free_pic(picture *pic) {
+    if (pic->type) {
+        for (int i = 0; i < pic->height; i++)
+            free(pic->pixels_rgb[i]);
+        free(pic->pixels_rgb);
+        free(pic);
+    }
+    else {
+        for (int i = 0; i < pic->height; i++)
+            free(pic->pixels[i]);
+        free(pic->pixels);
+        free(pic);
+    }
+}
+
+void write_picture(picture *image, char *filename, bool binary) {
+    FILE *fp = fopen(filename, "w");
+    if (image->type == 0) {
+        if (binary)
+            fprintf(fp, "P5\n");
+        else 
+            fprintf(fp, "P2\n");
+        
+        fprintf(fp, "%d %d\n", image->width, image->height);
+        fprintf(fp, "%d\n", image->value_max);
+        if (binary) {
+            for (int i = 0; i < image->height; i++) {
+                fwrite(image->pixels[i], 1, image->width, fp);
+            }
+        }
+        else {
+            for (int i = 0; i < image->height; i++) {
+                for (int j = 0; j < image->width; j++) {
+                    fprintf(fp, "%u\n", image->pixels[i][j]);
+                }
+            }
+        }
+    }
+    else {
+        if (binary)
+            fprintf(fp, "P6\n");
+        else 
+            fprintf(fp, "P3\n");
+        
+        fprintf(fp, "%d %d\n", image->width, image->height);
+        fprintf(fp, "%d\n", image->value_max);
+        if (binary) {
+            for (int i = 0; i < image->height; i++) {
+                fwrite(image->pixels_rgb[i], 3, image->width, fp);
+            }
+        }
+        else {
+            for (int i = 0; i < image->height; i++) {
+                for (int j = 0; j < image->width; j++) {
+                    RGB pix = image->pixels_rgb[i][j];
+                    fprintf(fp, "%u %u %u\n", pix.R, pix.G, pix.B);
+                }
+            }
+        }
+    }
+
+
+    fclose(fp);
+}
+
+
+double W0(double x) {
+    if (abs(x) > 0.5) 
+        return 0;
+    else if (abs(x) == 0.5)
+        return 1;
+    else
+        return 0.5;
+}
+
+double W1(double x) {
+    if ((0 <= x) && (x <= 1)) 
+        return 1-x;
+    else if ((-1 <= x) && (x <= 0))
+        return x+1;
+    else
+        return 0;
+}
+
+double W2(double x) {
+    if ((-1.5 <= x) && (x <= -0.5)) 
+        return 0.5 * pow(x+1.4, 2);
+    else if ((-0.5 <= x) && (x <= 0.5))
+        return 0.75-pow(x, 2);
+    else if ((0.5 <= x) && (x <= 1.5))
+        return 0.5 * pow(x-1.5, 2);
+    else
+        return 0;
+}
+
+double W3(double x) {
+    if ((0 <= abs(x)) && (abs(x) <= 1)) 
+        return 1/2*pow(abs(x),3)-pow(x, 2)+2/3; // not sure about priorities here could cause problems donw the road
+    else if ((1 <= abs(x)) && (abs(x) <= 2))
+        return 1/6*pow(2-abs(x), 3);
+    else
+        return 0;
+}
+
+double (*W[4]) (double);
+
+uchar interpolation_pgm(picture *image, double x, double y) {
+    uchar pixels[N+1][N+1];
+    //TODO handle edge pixels
+    if (N == 0) {
+        if ((round(x) >= image.height) || (round(y) >= image.width) || (round(x) < 0) || (round(y) < 0)) 
+            pixels[0][0] = 0;
+
+        else
+            pixels[0][0] = image->pixels[(int)round(x)][(int)round(y)];
+    }
+    if (N == 1) {
+        if ((int(x) >= image.height) || (int(y) >= image.width) || (int(x) < 0) {
+            
+        } 
+        else {
+            if ((int(y) < 0)) // this is hell
+                pixels[0][0] = image->pixels[(int)x][(int)y];
+            pixels[0][1] = image->pixels[(int)x][(int)y+1];
+        }
+        pixels[1][0] = image->pixels[(int)x+1][(int)y];
+        pixels[1][1] = image->pixels[(int)x+1][(int)y+1];
+    }
+    if (N == 2) {
+        pixels[0][0] = image->pixels[(int)x - 1][(int)y - 1];
+        pixels[0][1] = image->pixels[(int)x - 1][(int)y];
+        pixels[0][2] = image->pixels[(int)x - 1][(int)y + 1];
+
+        pixels[1][0] = image->pixels[(int)x][(int)y - 1];
+        pixels[1][1] = image->pixels[(int)x][(int)y];
+        pixels[1][2] = image->pixels[(int)x][(int)y + 1];
+
+        pixels[2][0] = image->pixels[(int)x + 1][(int)y - 1];
+        pixels[2][1] = image->pixels[(int)x + 1][(int)y];
+        pixels[2][2] = image->pixels[(int)x + 1][(int)y + 1];
+    }
+    if (N == 3) {
+        pixels[0][0] = image->pixels[(int)x - 1][(int)y - 1];
+        pixels[0][1] = image->pixels[(int)x - 1][(int)y];
+        pixels[0][2] = image->pixels[(int)x - 1][(int)y + 1];
+        pixels[0][3] = image->pixels[(int)x - 1][(int)y + 2];
+        
+
+        pixels[1][0] = image->pixels[(int)x][(int)y - 1];
+        pixels[1][1] = image->pixels[(int)x][(int)y];
+        pixels[1][2] = image->pixels[(int)x][(int)y + 1];
+        pixels[1][3] = image->pixels[(int)x][(int)y + 2];
+
+        pixels[2][0] = image->pixels[(int)x + 1][(int)y - 1];
+        pixels[2][1] = image->pixels[(int)x + 1][(int)y];
+        pixels[2][2] = image->pixels[(int)x + 1][(int)y + 1];
+        pixels[2][3] = image->pixels[(int)x + 1][(int)y + 2];
+
+        pixels[3][0] = image->pixels[(int)x + 2][(int)y - 1];
+        pixels[3][1] = image->pixels[(int)x + 2][(int)y];
+        pixels[3][2] = image->pixels[(int)x + 2][(int)y + 1];
+        pixels[3][3] = image->pixels[(int)x + 2][(int)y + 2];
+    }
+
+    double sum = 0;
+    for (int i = 0; i < N+1; i++) {
+        for (int j = 0; j < N+1; j++) {
+            if (N >= 2)
+                sum += pixels[i][j] * image->pixels[(int)x + i - 1][(int) y + j - 1];
+            else 
+                sum += pixels[i][j] * image->pixels[(int)x + i][(int) y + j];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    W[0] = W0;
+    W[1] = W1;
+    W[2] = W2;
+    W[3] = W3;
+
+    picture *img = get_picture("Monarch.pgm");
+    printf("%u\n", interpolation_pgm(img, 5, 5));
+    free_pic(img);
+}
